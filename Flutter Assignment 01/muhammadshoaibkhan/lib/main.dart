8d6b79e361c1dd810e873f05bc181d281990667e
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2021-05-10 14:34:25 +0500

    2nd Commit

---
+++ Flutter Assignment 01/muhammadshoaibkhan/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+      home: Scaffold(
+        body: Text("Muhammad Shoaib Khan\n shoaibmuhammad960@gmail.com"),
+      ),
+    );
+  }
+}
